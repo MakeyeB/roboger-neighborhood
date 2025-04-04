8f437d60229ae8f78a74c165c2137e654b0cab2a
MakeyeB <makeyebuchanan0617@gmail.com>
2025-04-04 11:55:30 -0700

    Add initial HTML file

--- js/scripts.js
+++ js/scripts.js
@@ -24,7 +24,7 @@ function changeNumber(number) {
 }
 
 function convertNumber(number) {
-  if (number <= 0) {
+  if (!number || number <= 0) {
     return false;
   }
   const convertedNumbers = [];
@@ -33,3 +33,38 @@ function convertNumber(number) {
   }
   return convertedNumbers;
 }
+
+// ui logic
+
+function createNewOutput() {
+  if (document.getElementById("output")) {
+    document.getElementById("output").remove();
+  }
+  const newOutput = document.createElement("div");
+  newOutput.id = "output";
+  document.querySelector("form").after(newOutput);
+  return newOutput;
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  const convertedInput = convertNumber(document.getElementById("input").value);
+  const newOutput = createNewOutput();
+  if (!convertedInput) {
+    const warning = document.createElement("p");
+    warning.innerText = "Input a valid number.";
+    newOutput.append(warning);
+    return;
+  }
+  const newList = document.createElement("ul");
+  convertedInput.forEach(function(value) {
+    const newItem = document.createElement("li");
+    newItem.innerText = value;
+    newList.append(newItem);
+  });
+  newOutput.append(newList);
+}
+
+window.addEventListener("load", function() {
+  this.document.querySelector("form").addEventListener("submit", onSubmit)
+});
